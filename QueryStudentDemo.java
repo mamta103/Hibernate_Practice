4ab965dcb4f14de2327c8652842753e6bd94f61f
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2019-04-06 00:01:15 +0530

    Hibernate Query details

--- QueryStudentDemo.java
+++ QueryStudentDemo.java
@@ -8,35 +8,73 @@ import org.hibernate.cfg.Configuration;
 
 import com.love2code.hibernate.entity.Student;
 
+
 public class QueryStudentDemo {
 
 	public static void main(String[] args) {
 
-		SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class)
-				.buildSessionFactory();
-		Session session = sessionFactory.getCurrentSession();
-		try {
-
-			// Create Object Corresponding POJO class
-			// Begin Transaction
-			// Save Object
-			// Closing Connection
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
 		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
 			session.beginTransaction();
 			
-			//query the student
-			List<Student> theStudents= session.createQuery("from Student").list();
-			//display the student
-			for(Student tempStudent:theStudents){
-				System.out.println(tempStudent);
-			}
+			// query students
+			List<Student> theStudents = session.createQuery("from Student").list();
+			
+			// display the students
+			displayStudents(theStudents);
+			
+			// query students: lastName='Doe'
+			theStudents = session.createQuery("from Student s where s.lastName='Doe'").list();
+			
+			// display the students
+			System.out.println("\n\nStudents who have last name of Doe");
+			displayStudents(theStudents);
+			
+			// query students: lastName='Doe' OR firstName='Daffy'
+			theStudents =
+					session.createQuery("from Student s where"
+							+ " s.lastName='Doe' OR s.firstName='Daffy'").list();
+			
+			System.out.println("\n\nStudents who have last name of Doe OR first name Daffy");
+			displayStudents(theStudents);
+			
+			// query students where email LIKE '%gmail.com'
+			theStudents = session.createQuery("from Student s where"
+					+ " s.email LIKE '%gmail.com'").list();
+
+			System.out.println("\n\nStudents whose email ends with gmail.com");			
+			displayStudents(theStudents);
+			
+			
+			// commit transaction
 			session.getTransaction().commit();
+			
 			System.out.println("Done!");
-
-		} finally {
-			sessionFactory.close();
 		}
+		finally {
+			factory.close();
+		}
+	}
 
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 }
+
+
+
+
+
