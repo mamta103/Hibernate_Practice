9608186bf07bf8e7ce579ea814b5f854f97726c3
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2019-04-08 18:07:22 +0530

    216. @OneToOne - Bi-Directional - Develop Mai…
    216. @OneToOne -
    Bi-Directional - Develop Mai…

---
+++ com/love2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,47 @@
+package com.love2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			int theId=3;
+			InstructorDetail  tempInstructorDetail= (InstructorDetail) session.get(InstructorDetail.class, theId);
+			
+			
+			//print the instructor detail
+			
+			System.out.println("tempInstructorDetail ::"+tempInstructorDetail);
+			
+			//print the associated instructor
+			System.out.println("the associated Instructor :: "+tempInstructorDetail.getInstructor());
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
---
+++ com/love2code/jdbc/TestJdbc.java
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ */
+package com.love2code.jdbc;
+
+import java.sql.DriverManager;
+
+/**
+ * @author Mamta
+ *
+ */
+public class TestJdbc {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// String jdbcURL =
+		// "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+
+		String username = "hbstudent";
+		String password = "hbstudent";
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+
+			DriverManager.getConnection(jdbcUrl, username, password);
+			System.out.println("Connection successful!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
